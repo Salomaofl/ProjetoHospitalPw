65fedcb0b40342bcc83150f130ce5967cd4f66f8
richard-estar <88118947+richard-estar@users.noreply.github.com>
2023-05-31 22:15:22 -0400

    complete

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import {Button} from "react-bootstrap";
 import {useNavigate} from "react-router-dom"
+import { Outlet, Link } from "react-router-dom";
+
+
+
 
 const Home = () => {
     let navigate = useNavigate()
     return (
         <div>
             Home
-            <Button onClick={()=>navigate('/form')}></Button>
+            <Button onClick={()=>navigate('/form')}>Form</Button>
+            <Outlet />
         </div>
     );
 };
